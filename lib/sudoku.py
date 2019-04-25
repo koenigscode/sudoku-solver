4b56e5834aea12e06cefbd1ce65793465900cad1
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-04-25 19:03:51 +0200

    Add validation methods

--- lib/sudoku.py
+++ lib/sudoku.py
@@ -17,6 +17,25 @@ class Sudoku:
     def from_file(path):
         pass
 
+    def is_valid_state(self):
+        for row, col in np.ndindex(self.sudoku.shape):
+            val = self.sudoku[row, col]  # current value
+            # number of the value's occurences in the current row
+            row_count = sum(self.sudoku[row] == val)
+            # number of the value's occurences in the current column
+            col_count = sum(self.sudoku[:, col] == val)
+
+            return row_count == 1 and col_count == 1 and self._has_valid_values()
+
+    def is_finished(self):
+        return self.is_valid_state() and sum(self.sudoku.flatten() == 0) == 0
+
+    def _has_valid_values(self):
+        for val in self.sudoku.flatten():
+            if val < 0 or val > self.sudoku.shape[0]**2:  # invalid number
+                return False
+
+        return True
     @staticmethod
     def _has_valid_shape(shape):
         x_size, y_size = shape
