361a0a1423cd1a5009eb950c18552df307bd0cb9
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-04-27 19:29:55 +0200

    Add docstrings

--- lib/sudoku.py
+++ lib/sudoku.py
@@ -12,6 +12,14 @@ colorama.init()
 class Sudoku:
 
     def __init__(self, values: np.array, box_size: int):
+        """Create a new Sudoku object.
+
+        Args:
+            values (numpy.array): array to create the sudoku from
+                empty fields should be set to 0
+            box_size (int): box size (length) of the sudoku
+                most sudokus are 9x9 and therefore have a boxsize of 3
+        """
         if not self._is_valid_shape(values.shape):
             raise ValueError("x size doesn't equal y size")
 
@@ -22,15 +30,35 @@ class Sudoku:
         self.box_size = box_size
 
     def first_empty(self):
+        """Get the position of the first empty value (first zero) in the sudoku
+
+        Returns:
+            tuple containing index of row and index of column
+            or None if there is no empty value
+        """
         for irow, row in enumerate(self.values):
             col_pos = np.where(row == 0)[0]
             if len(col_pos) > 0:
                 return irow, np.where(row == 0)[0][0]
 
     def is_finished(self):
+        """Check if the sudoku is finished and valid
+
+        Returns:
+            true if sudoku has valid state and there are no empty values
+                (zeros) left
+        """
         return self.has_valid_state() and sum(self.values.flatten() == 0) == 0
 
     def has_valid_state(self):
+        """Check if the sudoku has a valid state, so every value only occurs
+            once in a row and column
+            empty values (zeros) are allowed
+
+        Returns:
+            True if all values occur in their row and column only once,
+            otherwise False
+        """
         if not self.has_valid_values():
             return False
 
@@ -46,6 +74,12 @@ class Sudoku:
         return True
 
     def has_valid_values(self):
+        """Check if all of the sudoku's values are valid
+            zero is counted as a valid value
+
+        Returns:
+            True if all values are within the sudoku's range, otherwise False
+        """
         for val in self.values.flatten():
             if val < 0 or val > self.box_size**2:  # invalid number
                 return False
@@ -53,6 +87,15 @@ class Sudoku:
         return True
 
     def solve(self, extend=None):
+        """Solve the sudoku
+        Args:
+            extended (list): return the sudoku as a formatted string
+                instead of the solved sudoku
+                contains different options in form of a string within the list:
+                "original": show the original and solved sudoku;
+                    also label the original and saved one
+                "timer": show the time taken to solve the sudoku
+        """
         if type(extend) != list:
             return self._solve()
 
@@ -65,7 +108,7 @@ class Sudoku:
             return "Could not be solved"
 
         if "original" in extend:
-        res += f"Original:\n{self}\nSolved:\n{solved}\n"
+            res += f"Original:\n{self}\nSolved:\n{solved}\n"
         else:
             res += solved.__repr__()
 
@@ -75,7 +118,12 @@ class Sudoku:
         return res, solved
 
     def _solve(self):
+        """Internal solve method to find the solution using recursion
 
+        Returns:
+            Sudoku if it could be solved
+            otherwise None
+        """
         if not self.has_valid_state():
             return None
         if self.first_empty() is None:
@@ -96,10 +144,22 @@ class Sudoku:
 
     @staticmethod
     def _is_valid_shape(shape):
+        """Check for a valid shape
+
+        Args:
+            shape (tuple): x and y size
+        """
         x_size, y_size = shape
         return x_size == y_size
 
     def __repr__(self, highlight=None):
+        """Visual representation of the sudoku
+
+        Args:
+            highlight (tuple): row index and column index indicating the value
+                to be highlighted
+                useful for debugging
+        """
         s = ""
         row_delimiter = "-" * \
             ((len(self.values)+len(self.values)//self.box_size)*2 + 1) + "\n"
