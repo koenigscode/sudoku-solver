babcb63ec1111f398b5202c19b9e63f805c75630
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-04-27 19:37:18 +0200

    Fix: Use existing example in main.py
    main.py was using example2.txt, which, however, was removed in a past commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import numpy as np
 from lib.sudoku import Sudoku
 
-input = np.loadtxt("examples/example2.txt")
+input = np.loadtxt("examples/example1.txt")
 sudoku = Sudoku(input, 3)
 
 print(sudoku.solve(extend=["original", "timer"])[0])
