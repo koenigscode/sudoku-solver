562edf5dbb60554a0084f8ba16121c97b04f421e
michaelkoenig2002 <koenigmichael2002@gmail.com>
2019-04-27 20:36:18 +0200

    Only execute main.py's content if it's the main file

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import numpy as np
 from lib.sudoku import Sudoku
 
-input = np.loadtxt("examples/example1.txt")
-sudoku = Sudoku(input, 3)
+if __name__ == "__main__":
+    # load example sudoku from file as a numpy array
+    input = np.loadtxt("examples/example1.txt")
+    # create sudoku from array and set box size to 3
+    sudoku = Sudoku(input, 3)
 
-print(sudoku.solve(extend=["original", "timer"])[0])
+    # solve sudoku and print it
+    print(sudoku.solve(extend=["original", "timer"])[0])
